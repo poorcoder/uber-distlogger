62b1959c06b2afbbc5e64b2650e5655f2fc2bea2
Kiran Ryali <kiran.ryali@gmail.com>
2011-05-26 22:41:19 -0700

    finished test responses

--- test/app.test.js
+++ test/app.test.js
@@ -7,7 +7,8 @@
 
 var app = require('../app')
   , assert = require('assert');
-
+var mongoose = require('mongoose'); 
+var Log = mongoose.model('Log');
 
 console.log("Running tests...");
 module.exports = {
@@ -26,8 +27,6 @@ module.exports = {
       undefined);
   },
   'WARN_MOBILE': function(){
-      var mongoose = require('mongoose');
-      var Log = mongoose.model('Log');
       assert.response(app,
       { url: '/logs/new' ,
         method: 'POST', 
@@ -45,6 +44,17 @@ module.exports = {
       },
       {},
          function(res){
+            Log.findOne({
+               time: 1306469069405,
+               level: 'WARN',
+               ip: '127.0.0.1',
+               line_num: 'dispatch_server.js:228:2',
+               body: 'Permission to API call denied',
+               component: 'mobile'
+               }, 
+               function(err, docs){
+                  assert.isNotNull(docs, "WARN MOBILE");
+            });
          }
       );
   },
@@ -59,12 +69,24 @@ module.exports = {
             level: 'INFO',
             ip: '127.0.0.1',
             line_num: '',
-            body: 'Client received dispatch'
+            body: 'Client received dispatch',
+            component: 'mobile'
            }
         )
       },
       {},
          function(res){
+            Log.findOne({
+               time: 1306469031801,
+               level: 'INFO',
+               ip: '127.0.0.1',
+               line_num: '',
+               body: 'Client received dispatch',
+               component: 'mobile'
+               }, 
+               function(err, docs){
+                  assert.isNotNull(docs, "INFO MOBILE");
+            });
          }
       );
   },
@@ -86,6 +108,17 @@ module.exports = {
       },
       {},
          function(res){
+            Log.findOne({
+               time: 1306469804010,
+               level: 'ERROR',
+               ip: '127.0.0.1',
+               line_num: 'http_client.js:159:20',
+               body: 'EINVAL, Invalid argument to rest.ubercab.com:80/:  <?xml version="1.0" encoding="utf-8" ?><ubercab version="1.0.41" token="jfE3nRNElvKAfIMUa5IhUgb82W7mXhqW4c882df9c7e822.61623787" app="driver" device="iphone"><type><msg_type>location</msg_type><location latitude="37.522963" longitude="-122.266399"></location><location_timestamp>2011-05-25 02:00:28</location_timestamp><location_timestamp_utc>2011-05-25 09:00:28</location_timestamp_utc><speed>27.852998</speed></type></ubercab>.  STACKTRACE:  Error    at ClientRequest.<anonymous> (/home/uber/dispatch/lib/http_client.js:159:145)    at ClientRequest.emit (events.js:64:17)    at Socket.<anonymous> (http.js:1214:11)    at Socket.emit (events.js:64:17)    at Array.0 (net.js:830:27)    at EventEmitter._tickCallback (node.js:126:26)',
+               component: 'dispatch'
+               }, 
+               function(err, docs){
+                  assert.isNotNull(docs, "ERROR DISPATCH");
+            });
          }
       );
   }

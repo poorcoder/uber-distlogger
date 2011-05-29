e1a4db8661564e2ee75d6e2069a86ea9e38763fb
Kiran Ryali <kiran.ryali@gmail.com>
2011-05-29 13:39:05 -0700

    moved file

---
+++ rules/index.js
@@ -0,0 +1,23 @@
+module.exports = [
+   {
+      email: 'kiran.ryali@gmail.com', 
+      filter: {
+         level: 'WARN',
+         component: 'mobile'
+      }
+   },
+   {
+      email: 'engineer2@uber.com', 
+      filter: {
+         level: 'ERROR',
+         component: 'dispatch'
+      }
+   },
+   {
+      email: 'enginee32@uber.com', 
+      filter: {
+         level: 'ERROR',
+         component: 'other'
+      }
+   },
+];

e9fdd666b6addb7f87d51cbbdab0f3191a9550ba
Kiran Ryali <kiran.ryali@gmail.com>
2011-06-04 16:39:31 -0700

    Added writes for file system, still needs to be tested

--- models/log.js
+++ models/log.js
@@ -1,3 +1,22 @@
+var fs = require('fs');
+
+var log_dir = '../logs';
+
+var fs_all;
+
+fs.open(log_dir + "/all", 'a+', function(err, fd){
+   fs_all = fd;
+});
+
+
+var writeQueue = {
+   push: function(){
+
+   }
+};
+
+var files = {};
+
 var LogModel = new Schema({
    time: Date,
    level: {type: String, index: true},
@@ -9,6 +28,11 @@ var LogModel = new Schema({
 });
 
 var Log = mongoose.model('Log', LogModel);
+
+var file_append = function(fd, string){
+   fs.write(fd, string, 0, string.length, null);
+};
+
 Log.prototype.toString = function(){
    var log = this['doc'];
    s = "";
@@ -18,5 +42,22 @@ Log.prototype.toString = function(){
    };
    return s;
 }
+
+Log.prototype.fs_write(){
+   var log = this['doc'];
+   var log_string = this.toString();
+
+   file_append(fs_write, log_string);
+
+   if( files[log.component] == undefined){
+      fs.open(log_dir + '/' + log.component,  'a+', function(err,fd){
+         files[log.component] = fd;
+         append(files[log.component], log_string); 
+      });
+   } else {
+      append(files[log.component], log_string); 
+   }
+
+};
 
 module.exports = Log;
